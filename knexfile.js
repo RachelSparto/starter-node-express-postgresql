c7f805fabd583575d938cc6a598e49ed04b57485
RachelSparto <rmbachman2016@hotmail.com>
2022-03-16 13:24:54 -0400

    seedign data and migrations added

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,7 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 
+const path = require("path");
 require("dotenv").config();
 const { DATABASE_URL } = process.env;
 
@@ -11,5 +12,11 @@ module.exports = {
   development: {
     client: "postgresql",
     connection: DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
   },
 };
